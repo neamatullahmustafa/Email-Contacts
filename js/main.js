47cb88153b56ab300851112634d8e7137aaf3866
neamatullahmustafa <tareqshrouq@gmail.com>
2023-12-28 17:35:13 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,26 @@
+function display() {
+  let httpUrl = new XMLHttpRequest();
+  httpUrl.open("GET", "https://reqres.in/api/users?page=2");
+  httpUrl.send();
+  let emailData = [];
+
+  httpUrl.addEventListener("readystatechange", function () {
+    if (httpUrl.readyState == 4 && httpUrl.status == 200) {
+      container = "";
+
+      emailData = JSON.parse(httpUrl.response).data;
+      console.log(emailData);
+      for (let i = 0; i < JSON.parse(httpUrl.response).per_page; i++) {
+        container += ` <tr>
+      <th scope="row">${i + 1}</th>
+      <td><img src="${emailData[i].avatar}" alt=""></td>
+      <td>${emailData[i].first_name} ${emailData[i].last_name}</td>
+      <td>${emailData[i].email}</td>
+    </tr>`;
+        console.log(container);
+      }
+      document.getElementById("data").innerHTML = container;
+    }
+  });
+}
+display();
